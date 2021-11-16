3b5113c52274627b386553a2d65752048927ffe0
Łukasz Adamczyk <lukasz012798@gmail.com>
2021-11-16 20:09:27 +0100

    Broken slides fix

--- app.js
+++ app.js
@@ -20,9 +20,13 @@ function generateHex() {
 }
 
 function randomColors() {
+  initialColors = [];
+
   colorDivs.forEach((div, index) => {
     const hexText = div.children[0];
     const randomColor = generateHex();
+
+    initialColors.push(chroma(randomColor).hex());
 
     div.style.backgroundColor = randomColor;
     hexText.innerText = randomColor;
@@ -76,7 +80,7 @@ function hslControls(e) {
   const brightness = sliders[1];
   const saturation = sliders[2];
 
-  const bgColor = colorDivs[index].querySelector("h2").innerText;
+  const bgColor = initialColors[index];
 
   let color = chroma(bgColor)
     .set("hsl.s", saturation.value)
